3bc29dd8da26d504da8286402236634b4480e093
Dustin Farris <dustin@dustinfarris.com>
2014-07-23 21:40:36 -0400

    Update

--- config/environment.js
+++ config/environment.js
@@ -24,14 +24,16 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.APP.MY_ENVIRONMENT = 'DEVELOPMENT';
   }
 
   if (environment === 'test') {
-
+    ENV.APP.MY_ENVIRONMENT = 'TEST';
   }
 
   if (environment === 'production') {
-
+    ENV.APP.MY_ENVIRONMENT = 'PRODUCTION';
   }
 
   return ENV;
